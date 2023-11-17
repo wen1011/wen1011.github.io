26f7537d4861aaf44dfc497e8f5a39664ce08a71
Wen Yun Chiang <jennychiang2016@gmail.com>
2023-11-17 18:40:43 +0800

    20231116 server status loading

--- components/Hero/index.js
+++ components/Hero/index.js
@@ -9,7 +9,7 @@ import myRwdImage from "../../public/Rectangle 19trans.png";
 export default function Hero() {
   return (
     <>
-      <div className="lg:flex md:justify-between isolate hidden">
+      <div className="lg:flex md:justify-between isolate hidden  ">
         <div className="mx-auto max-w-xl py-36 sm:py-40 lg:py-36 z-10">
           <div className="mb-6">
             <div className="flex mb-6">
@@ -568,29 +568,33 @@ export default function Hero() {
             <div className="mt-6">
               <a
                 href="#"
-                className="rounded-full  bg-white px-3.5 py-2.5 text-sm font-medium text-[#3670C7] shadow-sm hover:bg-blue-500 hover:text-white focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                className="rounded-full  bg-white px-5 py-1.5 text-sm font-medium text-[#3670C7] shadow-sm hover:bg-blue-500 hover:text-white focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
               >
                 認識候選人<span aria-hidden="true"> → </span>
               </a>
             </div>
           </div>
         </div>
-        <div className="relative translate-x-1/4">
-          <Image src={myRwdImage} className="min-w-full" alt="" />
-        </div>
-        <div
-          className="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]"
-          aria-hidden="true"
-        >
-          <div
-            className="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]"
-            style={{
-              clipPath:
-                "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
-            }}
+
+        <div className="static">
+          <Image
+            src={myRwdImage}
+            className=" w-screen absolute z-10 translate-x-1/4 -translate-y-1/3 "
+            alt=""
           />
-          <div className="absolute top-[calc(100%-26rem)] translate-x-1/4">
-            <Image src={myRwdImage} className="min-w-full" alt="" />
+          <div className="bg-[#6386D1]  flex top-4 left-0 w-80 h-64" />
+          <div
+            className="absolute translate-y-1/2 translate-x-1/4  inset-x-0  -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-20rem)]"
+            aria-hidden="true"
+          >
+            <div className="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] bg-gradient-to-tr  opacity-30 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]" />
+            <div className="absolute sm:-top-[calc(100%-28rem)] top-[calc(100%-36rem)]  translate-x-1/5">
+              <Image
+                src={myRwdImage}
+                className="object-contain -translate-y-1/4 translate-x-1/3 "
+                alt=""
+              />
+            </div>
           </div>
         </div>
       </div>
